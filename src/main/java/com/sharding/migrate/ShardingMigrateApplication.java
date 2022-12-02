999b8d06dc676452336589df3d42350b2f8e48e6
chenlong <clbigdata@163.com>
2022-12-02 21:39:45 +0800

    项目初始化，project init

---
+++ src/main/java/com/sharding/migrate/ShardingMigrateApplication.java
@@ -0,0 +1,15 @@
+package com.sharding.migrate;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan(basePackages = {"com.sharding.migrate.mapper"})
+@SpringBootApplication
+public class ShardingMigrateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingMigrateApplication.class, args);
+    }
+
+}
