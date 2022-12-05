750158a84358a88996a17cb013620c60bec59e1f
chenlong <clbigdata@163.com>
2022-12-05 20:58:35 +0800

    controller

---
+++ src/main/java/com/sharding/migrate/controller/AdminController.java
@@ -0,0 +1,10 @@
+package com.sharding.migrate.controller;
+
+/**
+ * @ClassName AdminController
+ * @Author ：daiyu
+ * @Date 2022-12-05 20:29
+ * @Description：管理后台
+ */
+public class AdminController {
+}
---
+++ src/main/java/com/sharding/migrate/controller/MigrateController.java
@@ -0,0 +1,11 @@
+package com.sharding.migrate.controller;
+
+/**
+ * @ClassName MigrateController
+ * @Author ：daiyu
+ * @Date 2022-12-05 20:29
+ * @Description：迁移逻辑配置
+ */
+public class MigrateController {
+
+}
