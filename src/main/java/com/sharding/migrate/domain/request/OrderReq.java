b4a83cdee1876058020d791582f9a89602efe580
clbigdata <clbigdata@163.com>
2023-02-07 23:37:08 +0800

    添加查询

---
+++ src/main/java/com/sharding/migrate/domain/request/OrderReq.java
@@ -0,0 +1,10 @@
+package com.sharding.migrate.domain.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class OrderReq {
+    private String id;
+}
--- src/main/java/com/sharding/migrate/mapper/TOrderMapper.java
+++ src/main/java/com/sharding/migrate/mapper/TOrderMapper.java
@@ -2,6 +2,7 @@ package com.sharding.migrate.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.sharding.migrate.domain.TOrder;
+import com.sharding.migrate.domain.request.OrderReq;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 @Mapper
 public interface TOrderMapper extends BaseMapper<TOrder> {
     List<TOrder> selectList();
+
+    TOrder selectOne(OrderReq orderReq);
 }
